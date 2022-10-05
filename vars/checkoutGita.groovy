6549d6891def6aeb87f1e5dad7e544000027f437
StevaRmnk <79976258+StevaRmnk@users.noreply.github.com>
2022-10-05 12:17:55 +0200

    Update checkoutGita.groovy

--- vars/checkoutGita.groovy
+++ vars/checkoutGita.groovy
@@ -1,5 +1,5 @@
 def call(String odakleDaCheckoutuje,String linkGitrepozitorijuma,String grana,String odredisteCheckouta){
-  checkout([$class: 'GitSCM', branches: [[name: '*/${grana}']],
+  checkout([$class: 'GitSCM', branches: [[name: "*/${grana}"]],
             extensions: [], 
-            userRemoteConfigs: [[url: 'https://github.com/StevaRmnk/DevopsRepo']]])
+            userRemoteConfigs: [[url: "${linkGitrepozitorijuma}"]]])
 }
